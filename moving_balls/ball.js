df1543ae73000f415b4e7cd1a56a2b346c7761b2
Dmitry Popov <39777681+dimonrmp@users.noreply.github.com>
2024-01-15 15:16:14 -0500

    Add files via upload

---
+++ moving_balls/ball.js
@@ -0,0 +1,77 @@
+//Set global variable that would contain the position, velocity and the html element "ball"
+
+var balls = [];
+
+const ballsQty = 15;
+
+const velocity = 30;
+const Xmin = 0;
+const Xmax = window.innerWidth;
+const Ymin = 0;
+const Ymax = window.innerHeight;
+var i = 0;
+
+function createBalls() {
+    for (i = 0; i < ballsQty; ++i) {
+        let ballId = `ball${i}`;
+        const ball = document.createElement('div');
+        ball.setAttribute('id', ballId);
+        ball.className = 'ball';
+
+        document.body.appendChild(ball);
+
+        let randomWidth = 50 + Math.round(Math.random() * 150);
+
+        balls.push({
+            id: ballId,
+            reverseX: false,
+            reverseY: false,
+            positionX: 200 + Math.round(Math.random() * (Xmax - 400)),
+            positionY: 200 + Math.round(Math.random() * (Ymax - 400)),
+            width: randomWidth,
+            velocity: 30 - (randomWidth / 10)
+        })
+
+        ball.style.left = balls[i].positionX + 'px';
+        ball.style.top = balls[i].positionY + 'px';
+        ball.style.width = balls[i].width + 'px';
+        ball.style.height = ball.style.width;
+        ball.style.background = `RGB(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
+    }
+
+    console.log('balls created!', balls)
+};
+
+//write a function that can change the position of the html element "ball"
+function moveBalls() {
+
+    // let i = Math.round(Math.random() * 9);
+
+    if (i == ballsQty) i = 0;
+
+    var ball = document.getElementById(`ball${i}`);
+
+    balls[i].positionX += balls[i].reverseX ? -balls[i].velocity : balls[i].velocity;
+    balls[i].positionY += balls[i].reverseY ? -balls[i].velocity : balls[i].velocity;
+
+    if (balls[i].positionX >= Xmax - balls[i].width || balls[i].positionX <= Xmin) {
+        balls[i].reverseX = !balls[i].reverseX;
+        ball.style.background = `RGB(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
+    }
+
+    if (balls[i].positionY >= Ymax - balls[i].width || balls[i].positionY <= Ymin) {
+        balls[i].reverseY = !balls[i].reverseY;
+        ball.style.background = `RGB(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
+    }
+
+    ball.style.left = balls[i].positionX + 'px';
+    ball.style.top = balls[i].positionY + 'px';
+
+    ++i;
+
+}
+
+createBalls();
+
+// This call the moveBall function every 100ms
+setInterval(moveBalls, 1);
